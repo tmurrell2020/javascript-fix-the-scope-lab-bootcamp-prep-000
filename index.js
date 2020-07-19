3a5c20b5d52d534f52a48bf4a9761c1a55bae91a
Terrell Murrell <littlerell12@gmail.com>
2020-07-19 22:46:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + 2
+  var two = 2
+  return n + two
 
   // Feel free to move things around!
-  var two = 2
 }
 
 var funkyFunction = function() {
@@ -30,4 +30,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()
+function("Funky")
